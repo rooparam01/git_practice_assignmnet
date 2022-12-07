0f3caa30ddc4331e931b47e83565530f7714934f
rooparam01 <111178057+rooparam01@users.noreply.github.com>
2022-12-07 12:38:12 +0530

    firstFileUploadSucessfully

---
+++ PrimeorNot.js
@@ -0,0 +1,11 @@
+let num = 13;
+for (let i = 1; i <= num; i++) {
+  if (num % i == 0) {
+    count++;
+  }
+}
+if (count == 2) {
+  console.log("Prime Number");
+} else {
+  console.log("Not Prime");
+}
